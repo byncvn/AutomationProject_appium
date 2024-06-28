c069b291903568abdc108eccd9082f605ba18865
byncvn <byncvn@gmail.com>
2024-06-28 15:22:31 -0700

    add variables

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -21,7 +21,13 @@ public class BaseTest {
     public AndroidDriver androidDriver;
     public AppiumDriverLocalService appiumService;
     public UiAutomator2Options capabilities;
-    public URL url = new URI("").toURL();
+    public URL url = new URI("http://127.0.0.1:4723/").toURL();
+    public String pathMainJS = "C:\\Users\\ggeta\\AppData\\Roaming\\npm\\node_modules\\appium\\build\\lib\\main.js";
+
+    public String deviceName = "SM-G975U";
+    public String appActivity = "com.swaglabsmobileapp.MainActivity";
+    public String appPackage = "com.swaglabsmobileapp";
+    public String appPath = "C:\\Users\\ggeta\\IdeaProjects\\AutomationProject_appium\\src\\test\\java\\resources\\androidApp\\SampleApp.apk";
 
     @BeforeClass
     public void setUp() throws MalformedURLException {
@@ -37,10 +43,10 @@ public class BaseTest {
 
     private void configureAndroidDriver() throws MalformedURLException {
         capabilities = new UiAutomator2Options();
-        capabilities.setDeviceName("");
-        capabilities.setApp("");
-        capabilities.setAppActivity("");
-        capabilities.setAppPackage("");
+        capabilities.setDeviceName(deviceName);
+        capabilities.setApp(appPath);
+        capabilities.setAppActivity(appActivity);
+        capabilities.setAppPackage(appPackage);
 
         androidDriver = new AndroidDriver(url,capabilities);
 
@@ -50,8 +56,8 @@ public class BaseTest {
 
     private void configureAppiumService() {
         appiumService = new AppiumServiceBuilder()
-                .withAppiumJS(new File(""))
-                .withIPAddress("")
+                .withAppiumJS(new File(pathMainJS))
+                .withIPAddress("127.0.0.1")
                 .usingPort(4723)
                 .build();
         appiumService.start();
