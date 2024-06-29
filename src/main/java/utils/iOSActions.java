d602c0ad0d4a11d0706c679ae671f6ddb66a85b3
byncvn <byncvn@gmail.com>
2024-06-29 03:33:56 -0700

    Update iOSActions.java
    Adding methods

--- src/main/java/utils/iOSActions.java
+++ src/main/java/utils/iOSActions.java
@@ -1,4 +1,51 @@
 package utils;
 
-public class iOSActions extends AppiumUtils{
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.ios.IOSDriver;
+import org.openqa.selenium.WebElement;
+
+public class iOSActions extends AppiumUtils {
+
+    IOSDriver driver;
+
+    public iOSActions(AppiumDriver driver) {
+        super(driver);
+    }
+
+    public void tapElement(WebElement webElement) {
+        webElement.click();
+        //TODO: Add log to inform element has been tapped
+        System.out.println("element" + webElement + " has been tapped");
+    }
+
+    public void sendKeysTo(WebElement webElement, String inputs) {
+        webElement.sendKeys(inputs);
+        driver.hideKeyboard();
+        //TODO: Add log to inform element has been tapped
+        System.out.println("input " + inputs + " has been sent");
+    }
+
+    public void scrollDown() {
+        //TODO:
+
+    }
+
+    public void scrollUp() {
+        //TODO:
+
+    }
+
+    public void scrollToText(){
+
+    }
+
+    public void swipeLeft() {
+        //TODO:
+
+    }
+
+    public void swipeRight() {
+        //TODO:
+
+    }
 }
