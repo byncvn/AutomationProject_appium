99f3ed95881292caac6e87fef83d8e8365923d1e
byncvn <byncvn@gmail.com>
2024-06-29 03:33:23 -0700

    adding util methods

--- src/main/java/utils/AppiumUtils.java
+++ src/main/java/utils/AppiumUtils.java
@@ -1,4 +1,29 @@
 package utils;
 
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
 public class AppiumUtils {
+
+    AppiumDriver driver;
+
+    public AppiumUtils(AppiumDriver driver){
+        this.driver = driver;
+    }
+
+    public Double getFormattedAmount(String amount){
+        return Double.parseDouble(amount.substring(1));
+    }
+
+    public void waitForElementToAppear(WebElement element){
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.attributeContains(element, "text" , "cart"));
+    }
 }
