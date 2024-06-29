291300a9ad283a8bd5cde23568c74f2ed08aa6cd
byncvn <byncvn@gmail.com>
2024-06-29 00:17:52 -0700

    create loginPage frame

--- src/main/java/Pages/LoginPage.java
+++ src/main/java/Pages/LoginPage.java
@@ -1,4 +1,24 @@
 package Pages;
 
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.pagefactory.AndroidFindBy;
+import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
+
 public class LoginPage {
+
+    AndroidDriver androidDriver;
+
+    public LoginPage(AndroidDriver androidDriver) {
+        this.androidDriver = androidDriver;
+        PageFactory.initElements(new AppiumFieldDecorator(androidDriver),this);
+    }
+
+    @AndroidFindBy(xpath = "//android.widget.TextView[@text=\"LOGIN\"]")
+    private WebElement usernameField;
+
+    public void clickUsernameField(){
+        usernameField.click();
+    }
 }
