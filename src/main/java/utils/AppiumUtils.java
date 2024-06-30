b52f62bb4d583172a9e1aa548a4891744600074a
byncvn <byncvn@gmail.com>
2024-06-30 02:06:41 -0700

    create getJsonData method

--- src/main/java/utils/AppiumUtils.java
+++ src/main/java/utils/AppiumUtils.java
@@ -1,13 +1,21 @@
 package utils;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.appium.java_client.AppiumDriver;
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.time.Duration;
+import java.util.HashMap;
+import java.util.List;
 
 public class AppiumUtils {
 
@@ -25,6 +33,18 @@ public class AppiumUtils {
     public void waitForElementToAppear(WebElement element) {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
         wait.until(ExpectedConditions.attributeContains((element), "text", "cart"));
+    }
+
+    //System.getProperty("user.dir") + "\\src\\test\\java\\testData\\sampleApp.json"
+    public List<HashMap<String, String>> getJsonData(String jsonFilePath) throws IOException {
+        String jsonContent = FileUtils.readFileToString(new File(jsonFilePath), StandardCharsets.UTF_8);
+
+        ObjectMapper mapper = new ObjectMapper();
+        List<HashMap<String, String>> data = mapper.readValue(jsonContent,
+                new TypeReference<List<HashMap<String, String>>>() {
+                });
+
+        return data;
     }
 
 
